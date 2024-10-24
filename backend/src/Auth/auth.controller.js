b34ba9249978480f96e050554d8a588d1f3e8a83
Andrea Korunoska <akorunoska@setronica.com>
2024-10-25 01:17:51 +0200

    .gitignore modified

---
+++ backend/src/Auth/auth.controller.js
@@ -0,0 +1,46 @@
+import jwt from 'jsonwebtoken';
+import bcryptjs from 'bcryptjs';
+import UserModel from "../common/models/user.model.js";
+import {generateSecretToken} from "../common/util/secret_token.js";
+
+require('dotenv').config();
+
+
+export const registerPost = async (req, res) => {
+    const { name, lastName, email, password, birthday, interestedIn, pastTrips, upcomingTrips} = req.body;
+    try {
+        const newUser = new UserModel({
+            name: name,
+            lastName: lastName,
+            email: email,
+            password: password,
+            birthday: birthday,
+            interestedIn: interestedIn,
+            pastTrips: pastTrips,
+            upcomingTrips: upcomingTrips,
+        });
+        await newUser.save();
+        res.status(201).json({user: newUser});
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+}
+
+
+export const loginPost = async (req, res) => {
+    const expirationSeconds = 1800;
+
+    try{
+        const user = await UserModel.findOne({ email: req.body.email}).select('+password');
+
+        if(!user || !bcryptjs.compareSync(req.body.password, user.password)) {
+            res.status(500).render('auth/login', {error: 'Incorrect username or password'});
+            return;
+        }
+        const token = await generateSecretToken(user._id, user.email);
+        const expirationDate =Math.floor(Date.now() / 1000) + expirationSeconds;
+        res.status(201).json({user: user, token: token, expiresIn: expirationDate});
+    } catch (error) {
+        res.status(500).render('auth/login', {error: "Error logging in"});
+    }
+}
